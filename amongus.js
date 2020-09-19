fb2a2be7109debc2d1dddd70be598f4bf34d8c6d
Matt Strenk <strenkml@users.noreply.github.com>
2020-09-19 11:42:08 -0400

    Added bot status.  Removed database files

--- amongus.js
+++ amongus.js
@@ -49,6 +49,7 @@ client.on("error", console.error);
 
 client.on("ready", () => {
   console.log("Bot is ready!");
+  client.user.setActivity(`${config.prefix}play`, { type: "WATCHING" });
   // serverInfo.deleteAll();
   reset(true);
 });
@@ -68,53 +69,56 @@ client.on("message", (msg) => {
 });
 
 client.on("messageReactionAdd", async (reaction, user) => {
-  if (!user.bot) {
-    // If reaction is one of the colors
-    if (emojis.some((emoji) => emoji == reaction.emoji.name)) {
-      var guild = reaction.message.guild.id;
-      if (!serverInfo.isUser(guild, user.id)) {
-        serverInfo.addUser(
-          guild,
-          user.id,
-          reaction.message.member.nickname || user.username,
-          reaction.emoji.name
-        );
-        let newUsername = `[${reaction.emoji.name}] ${serverInfo.getUserOldNick(
-          guild,
-          user.id
-        )}`;
-        reaction.message.guild
-          .member(user)
-          .setNickname(newUsername, "Among Us Color")
-          .catch((reason) => {
-            user.send(
-              `**Permission Missing!**\n__If you're the server owner__\nI can't set the nickname to \`${newUsername}\`, so you will have to it manually.\n__If you're not the server owner__\nMake sure that my role is at the top of the role hierarchy.`
-            );
-            reaction.remove(user);
-          });
-      } else {
+  var guild = reaction.message.guild.id;
+  if (serverInfo.getChannel(guild) == reaction.message.id) {
+    if (!user.bot) {
+      // If reaction is one of the colors
+      if (emojis.some((emoji) => emoji == reaction.emoji.name)) {
+        if (!serverInfo.isUser(guild, user.id)) {
+          serverInfo.addUser(
+            guild,
+            user.id,
+            reaction.message.member.nickname || user.username,
+            reaction.emoji.name
+          );
+          let newUsername = `[${
+            reaction.emoji.name
+          }] ${serverInfo.getUserOldNick(guild, user.id)}`;
+          reaction.message.guild
+            .member(user)
+            .setNickname(newUsername, "Among Us Color")
+            .catch((reason) => {
+              user.send(
+                `**Permission Missing!**\n__If you're the server owner__\nI can't set the nickname to \`${newUsername}\`, so you will have to it manually.\n__If you're not the server owner__\nMake sure that my role is at the top of the role hierarchy.`
+              );
+              reaction.remove(user);
+            });
+        } else {
+          reaction.remove(user);
+        }
+      } else if (reaction.emoji.name === resetEmoji) {
+        reset(false, reaction.message.guild.id);
         reaction.remove(user);
       }
-    } else if (reaction.emoji.name === resetEmoji) {
-      reset(false, reaction.message.guild.id);
-      reaction.remove(user);
     }
   }
 });
 
 client.on("messageReactionRemove", async (reaction, user) => {
   var guild = reaction.message.guild.id;
-  if (!user.bot) {
-    if (emojis.some((emoji) => emoji == reaction.emoji.name)) {
-      if (serverInfo.isUser(guild, user.id)) {
-        if (serverInfo.getUserColor(guild, user.id) == reaction.emoji.name) {
-          reaction.message.guild
-            .member(user)
-            .setNickname(
-              serverInfo.getUserOldNick(guild, user.id),
-              "Removing Among Us Color"
-            );
-          serverInfo.deleteUser(guild, user.id);
+  if (serverInfo.getChannel(guild) == reaction.message.id) {
+    if (!user.bot) {
+      if (emojis.some((emoji) => emoji == reaction.emoji.name)) {
+        if (serverInfo.isUser(guild, user.id)) {
+          if (serverInfo.getUserColor(guild, user.id) == reaction.emoji.name) {
+            reaction.message.guild
+              .member(user)
+              .setNickname(
+                serverInfo.getUserOldNick(guild, user.id),
+                "Removing Among Us Color"
+              );
+            serverInfo.deleteUser(guild, user.id);
+          }
         }
       }
     }
