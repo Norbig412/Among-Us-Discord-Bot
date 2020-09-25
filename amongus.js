774045fc4d16bada126b716318eb199a952267b5
Matt Strenk <mstrenk12@gmail.com>
2020-09-25 04:23:34 -0400

    Fixed debug with checking with message the reaction is on

--- amongus.js
+++ amongus.js
@@ -73,59 +73,59 @@ client.on("message", (msg) => {
 
 client.on("messageReactionAdd", async (reaction, user) => {
   var guild = reaction.message.guild.id;
-  // if (serverInfo.getChannel(guild) == reaction.message.id) {
   if (!user.bot) {
-    // If reaction is one of the colors
-    if (emojis.some((emoji) => emoji == reaction.emoji.name)) {
-      if (!serverInfo.isUser(guild, user.id)) {
-        serverInfo.addUser(
-          guild,
-          user.id,
-          reaction.message.guild.member(user).displayName,
-          reaction.emoji.name
-        );
-        let newUsername = `[${reaction.emoji.name}] ${
-          reaction.message.guild.member(user).displayName
-        }`;
-        reaction.message.guild
-          .member(user)
-          .setNickname(newUsername, "Among Us Color")
-          .catch((reason) => {
-            user.send(
-              `**Permission Missing!**\n__If you're the server owner__\nI can't set the nickname to \`${newUsername}\`, so you will have to it manually.\n__If you're not the server owner__\nMake sure that my role is at the top of the role hierarchy.`
-            );
-            reaction.remove(user);
-          });
-      } else {
+    if (serverInfo.getMessage(guild) == reaction.message.id) {
+      // If reaction is one of the colors
+      if (emojis.some((emoji) => emoji == reaction.emoji.name)) {
+        if (!serverInfo.isUser(guild, user.id)) {
+          serverInfo.addUser(
+            guild,
+            user.id,
+            reaction.message.guild.member(user).displayName,
+            reaction.emoji.name
+          );
+          let newUsername = `[${reaction.emoji.name}] ${
+            reaction.message.guild.member(user).displayName
+          }`;
+          reaction.message.guild
+            .member(user)
+            .setNickname(newUsername, "Among Us Color")
+            .catch((reason) => {
+              user.send(
+                `**Permission Missing!**\n__If you're the server owner__\nI can't set the nickname to \`${newUsername}\`, so you will have to it manually.\n__If you're not the server owner__\nMake sure that my role is at the top of the role hierarchy.`
+              );
+              reaction.remove(user);
+            });
+        } else {
+          reaction.remove(user);
+        }
+      } else if (reaction.emoji.name === resetEmoji) {
+        reset(false, reaction);
         reaction.remove(user);
       }
-    } else if (reaction.emoji.name === resetEmoji) {
-      reset(false, reaction);
-      reaction.remove(user);
     }
   }
-  // }
 });
 
 client.on("messageReactionRemove", async (reaction, user) => {
   var guild = reaction.message.guild.id;
-  // if (serverInfo.getChannel(guild) == reaction.message.id) {
   if (!user.bot) {
-    if (emojis.some((emoji) => emoji == reaction.emoji.name)) {
-      if (serverInfo.isUser(guild, user.id)) {
-        if (serverInfo.getUserColor(guild, user.id) == reaction.emoji.name) {
-          reaction.message.guild
-            .member(user)
-            .setNickname(
-              serverInfo.getUserOldNick(guild, user.id),
-              "Removing Among Us Color"
-            );
-          serverInfo.deleteUser(guild, user.id);
+    if (serverInfo.getMessage(guild) == reaction.message.id) {
+      if (emojis.some((emoji) => emoji == reaction.emoji.name)) {
+        if (serverInfo.isUser(guild, user.id)) {
+          if (serverInfo.getUserColor(guild, user.id) == reaction.emoji.name) {
+            reaction.message.guild
+              .member(user)
+              .setNickname(
+                serverInfo.getUserOldNick(guild, user.id),
+                "Removing Among Us Color"
+              );
+            serverInfo.deleteUser(guild, user.id);
+          }
         }
       }
     }
   }
-  // }
 });
 
 async function sendConfigMessage(guild, channel) {
