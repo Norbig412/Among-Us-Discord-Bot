2c7fa9d6f2bac5a823346afe089da8bd1283ec54
Matt Strenk <mstrenk12@gmail.com>
2020-09-26 18:05:18 -0400

    Cleaned up serverInfo

--- helpers/serverInfo.js
+++ helpers/serverInfo.js
@@ -3,147 +3,124 @@ const Enmap = require("enmap");
 const store = new Enmap({ name: config.dataStoreName });
 
 // Getters
-function getGuild(guild) {
-  if (isGuild(guild)) {
+module.exports.getGuild = (guild) => {
+  if (this.isGuild(guild)) {
     return store.get(`${guild}`);
   } else {
     return null;
   }
-}
+};
 
-function getMessage(guild) {
-  if (isGuild(guild)) {
+module.exports.getMessage = (guild) => {
+  if (this.isGuild(guild)) {
     return store.get(`${guild}`, "message");
   } else {
     return null;
   }
-}
+};
 
-function getChannel(guild) {
-  if (isGuild(guild)) {
+module.exports.getChannel = (guild) => {
+  if (this.isGuild(guild)) {
     return store.get(`${guild}`, "channel");
   } else {
     return null;
   }
-}
+};
 
-function getUser(guild, user) {
-  if (isGuild(guild) && isUser(guild, user)) {
+module.exports.getUser = (guild, user) => {
+  if (this.isGuild(guild) && this.isUser(guild, user)) {
     return store.get(`${guild}`, `users.${user}`);
   } else {
     return null;
   }
-}
+};
 
-function getUsers(guild) {
-  if (isGuild(guild)) {
+module.exports.getUsers = (guild) => {
+  if (this.isGuild(guild)) {
     return store.get(`${guild}`, "users");
   } else {
     return null;
   }
-}
+};
 
-function getUserOldNick(guild, user) {
-  if (isGuild(guild) && isUser(guild, user)) {
+module.exports.getUserOldNick = (guild, user) => {
+  if (this.isGuild(guild) && this.isUser(guild, user)) {
     return store.get(`${guild}`, `users.${user}.oldNick`);
   } else {
     return null;
   }
-}
+};
 
-function getUserColor(guild, user) {
-  if (isGuild(guild) && isUser(guild, user)) {
+module.exports.getUserColor = (guild, user) => {
+  if (this.isGuild(guild) && this.isUser(guild, user)) {
     return store.get(`${guild}`, `users.${user}.color`);
   } else {
     return null;
   }
-}
+};
 
-function getKeys() {
+module.exports.getKeys = () => {
   return store.indexes;
-}
+};
 
 // Setters
-function addGuild(guild) {
-  if (!isGuild(guild)) {
+module.exports.addGuild = (guild) => {
+  if (!this.isGuild(guild)) {
     store.set(`${guild}`, config.templates.serverInfo);
   }
-}
+};
 
-function setMessage(guild, message) {
-  if (isGuild(guild)) {
+module.exports.setMessage = (guild, message) => {
+  if (this.isGuild(guild)) {
     store.set(`${guild}`, message, "message");
   }
-}
+};
 
-function setChannel(guild, channel) {
-  if (isGuild(guild)) {
+module.exports.setChannel = (guild, channel) => {
+  if (this.isGuild(guild)) {
     store.set(`${guild}`, channel, "channel");
   }
-}
+};
 
-function addUser(guild, user, oldNick, color) {
-  if (isGuild(guild)) {
+module.exports.addUser = (guild, user, oldNick, color) => {
+  if (this.isGuild(guild)) {
     var newUser = config.templates.user;
     newUser.oldNick = oldNick;
     newUser.color = color;
     store.set(`${guild}`, newUser, `users.${user}`);
   }
-}
+};
 
-function setUserOldNick(guild, user, oldNick) {
-  if (isGuild(guild) && isUser(guild, user)) {
+module.exports.setUserOldNick = (guild, user, oldNick) => {
+  if (this.isGuild(guild) && this.isUser(guild, user)) {
     store.set(`${guild}`, oldNick, `users.${user}.oldNick`);
   }
-}
+};
 
-function setUserColor(guild, user, color) {
-  if (isGuild(guild) && isUser(guild, user)) {
+module.exports.setUserColor = (guild, user, color) => {
+  if (this.isGuild(guild) && this.isUser(guild, user)) {
     store.set(`${guild}`, color, `users.${user}.color`);
   }
-}
+};
 
 // Check Exists
-function isGuild(guild) {
+module.exports.isGuild = (guild) => {
   return store.has(guild);
-}
+};
 
-function isUser(guild, user) {
+module.exports.isUser = (guild, user) => {
   return store.has(`${guild}`, `users.${user}`);
-}
+};
 
 // Deleters
-function deleteGuild(guild) {
+module.exports.deleteGuild = (guild) => {
   store.delete(`${guild}`);
-}
+};
 
-function deleteUser(guild, user) {
+module.exports.deleteUser = (guild, user) => {
   store.delete(`${guild}`, `users.${user}`);
-}
+};
 
-function deleteAll() {
+module.exports.deleteAll = () => {
   store.deleteAll();
-}
-
-module.exports.getChannel = getChannel;
-module.exports.getMessage = getMessage;
-module.exports.getGuild = getGuild;
-module.exports.getUser = getUser;
-module.exports.getUsers = getUsers;
-module.exports.getUserOldNick = getUserOldNick;
-module.exports.getUserColor = getUserColor;
-module.exports.getKeys = getKeys;
-
-module.exports.addUser = addUser;
-module.exports.addGuild = addGuild;
-module.exports.setMessage = setMessage;
-module.exports.setChannel = setChannel;
-module.exports.setUserOldNick = setUserOldNick;
-module.exports.setUserColor = setUserColor;
-
-module.exports.isGuild = isGuild;
-module.exports.isUser = isUser;
-
-module.exports.deleteGuild = deleteGuild;
-module.exports.deleteUser = deleteUser;
-module.exports.deleteAll = deleteAll;
+};
